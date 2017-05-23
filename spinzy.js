a09336e80f5a23216e1dce5cdcce59a61092ea80
Anush <kadoyan@usc.edu>
2017-05-22 19:58:48 -0700

    added points

--- spinzy.js
+++ spinzy.js
@@ -1,6 +1,27 @@
 $(document).ready( function() {
+	
+	document.ontouchmove = function(event){
+    	
+    	event.preventDefault();
+	}
+
+	var points = 0;
+	var rate = 1/100;
+	
+	
 	$('#spinny').propeller({
 		inertia: 0.995, 
-		speed: 0
+		speed: 0,
+		onRotate:function() {
+			
+			if(this.speed>0.01) {
+				
+				points += Math.abs(this.speed) * rate;
+				
+				$('#points-value').text(Math.round(points));
+
+			}
+
+		}
 	});
 });
